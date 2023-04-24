21f8eb51467c35f82870ce246fede3b474d2f131
rafaelnunespe <rafael13021994@hotmail.com>
2023-04-24 18:13:39 -0300

    Upload de arquivo

---
+++ Projeto3/DiaValor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto3
+{
+    public class DiaValor
+    {
+        public int dia { get; set; }
+        public double valor { get; set; }
+    }
+}
---
+++ Projeto3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Projeto3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = File.ReadAllText("C:\\dados.json");
+
+            var listaDiaValor = JsonConvert.DeserializeObject<List<DiaValor>>(jsonString);
+
+            double min = double.MaxValue;
+            double max = double.MinValue;
+            double total = 0;
+            foreach (var diaValor in listaDiaValor)
+            {
+                if (diaValor.valor < min)
+                {
+                    min = diaValor.valor;
+                }
+                if (diaValor.valor > max)
+                {
+                    max = diaValor.valor;
+                }
+                total += diaValor.valor;
+            }
+
+            // Calcular o valor médio
+            double valorMedio = total / listaDiaValor.Count;
+
+            // Contar quantas vezes o valor ficou acima da média
+            int count= 0;
+            foreach (var diaValor in listaDiaValor)
+            {
+                if (diaValor.valor > valorMedio)
+                {
+                    count++;
+                }
+            }
+
+            // Imprimir os resultados
+            Console.WriteLine($"Valor mínimo: {min}");
+            Console.WriteLine($"Valor máximo: {max}");
+            Console.WriteLine($"Quantidade de valores acima da média: {count}");
+            Console.Read();
+        }
+    }
+}
